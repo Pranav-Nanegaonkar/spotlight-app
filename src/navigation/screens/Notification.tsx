3e085f5dd07883d4d0b429e241d59dead7a7f5fa
Pranav Nanegaonkar <nanegaonkarpranav68@gmail.com>
2026-01-14 00:11:48 +0530

    useUser hook in cleark in notificaiton

--- src/navigation/screens/Notification.tsx
+++ src/navigation/screens/Notification.tsx
@@ -1,10 +1,19 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { useUser } from "@clerk/clerk-expo";
 
 export default function NotificationScreen({ navigation }: any) {
+  const { isLoaded, isSignedIn, user } = useUser();
+
+  // console.log("-----------------USER: ", user);
+
   return (
     <View>
-      <Text onPress={() => navigation.goBack()}>Notification Screen</Text>
+      {isLoaded && user && (
+        <View>
+          <Text>{user.fullName}</Text>
+        </View>
+      )}
     </View>
   );
 }
